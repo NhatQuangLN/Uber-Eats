5b8086511298015f9516855a57e9fec6acf5f77d
NhatQuangLN <nhatquangfptu@gmail.com>
2022-06-30 10:04:22 +0700

    headerTabs UI, click effect

---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {View, Text, SafeAreaView} from 'react-native';
+import HeaderTabs from "../components/HeaderTabs";
+export default function Home(){
+    return (
+        <SafeAreaView>
+            <HeaderTabs/>
+        </SafeAreaView>
+    )
+}
